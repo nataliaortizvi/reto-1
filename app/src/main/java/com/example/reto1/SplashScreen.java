7d2699364748ca3d0466a1f235bf87f351974f8e
matheusvidartesandoval <math.vidarte@hotmail.com>
2021-10-28 19:57:04 -0500

    SplashScreen done

---
+++ app/src/main/java/com/example/reto1/SplashScreen.java
@@ -0,0 +1,63 @@
+package com.example.reto1;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class SplashScreen extends AppCompatActivity {
+
+    private boolean isAlive;
+
+    public static final int PERMISSION_CALLBACK = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        requestPermissions(new String[] {
+                Manifest.permission.CAMERA,
+                Manifest.permission.READ_EXTERNAL_STORAGE
+        }, PERMISSION_CALLBACK);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == PERMISSION_CALLBACK) {
+            boolean allGranted = true;
+            for (int r : grantResults) {
+                if (r == PackageManager.PERMISSION_DENIED) {
+                    allGranted = false;
+                }
+                if (allGranted) {
+                    Intent intent = new Intent(this, MainActivity.class);
+                    startActivity(intent);
+                } else {
+                    Toast.makeText(this, "Para una mejor experiencia acepta los permisos", Toast.LENGTH_SHORT).show();
+                    new Thread (
+                            () -> {
+                                while (isAlive) {
+                                    try {
+                                        Thread.sleep(8000);
+                                        Intent intent = new Intent(this, MainActivity.class);
+                                        startActivity(intent);
+                                    } catch (Exception e) {
+                                        e.printStackTrace();
+                                    }
+                                }
+                            }
+                    );
+                    Intent intent = new Intent(this, MainActivity.class);
+                    startActivity(intent);
+                }
+            }
+        }
+    }
+}
